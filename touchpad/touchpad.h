eeff44e8a897a7390483e358c7b3d2a127a31203
Stepan Snigirev <snigirev.stepan@gmail.com>
2019-01-30 02:38:20 +0100

    init

---
+++ touchpad/touchpad.h
@@ -0,0 +1,40 @@
+/**
+ * @file indev.h
+ *
+ */
+
+#ifndef INDEV_H
+#define INDEV_H
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdbool.h>
+#include <stdint.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void touchpad_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+ #ifdef __cplusplus
+ } /* extern "C" */
+ #endif
+
+#endif
